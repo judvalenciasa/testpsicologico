3881cdf501e44423c526f83e6c84c99c2d19057b
judvalenciasa <judvalenciasa@gmail.com>
2024-09-08 12:06:43 -0500

    adicion de atributos en la tabla reporte

--- app/Http/Controllers/ReportesController.php
+++ app/Http/Controllers/ReportesController.php
@@ -34,6 +34,36 @@ class ReportesController extends Controller
     }
 
 
+
+    /*
+'conocimiento_procedimental-1' => '4',
+  'planificacion-1' => '3',
+  'planificacion-2' => '3',
+  'planificacion-3' => '5',
+  'planificacion-4' => '5',
+  'planificacion-5' => '2',
+  'organizacion-7' => '2',
+  'organizacion-8' => '3',
+  'organizacion-9' => '5',
+  'organizacion-10' => '4',
+  'organizacion-11' => '4',
+  'monitoreo-12' => '4',
+  'monitoreo-13' => '2',
+  'monitoreo-14' => '5',
+  'monitoreo-15' => '2',
+  'monitoreo-16' => '3',
+  'depuracion-17' => '4',
+  'depuracion-18' => '4',
+  'depuracion-19' => '5',
+  'depuracion-20' => '1',
+  'depuracion-21' => '2',
+  'evaluacion-22' => '3',
+  'evaluacion-23' => '4',
+  'evaluacion-24' => '1',
+  'evaluacion-25' => '1',
+  'evaluacion-26' => '5',
+    */
+
     /**
      * Crea el reporte, con los siguientes parametros de ingreso
      * 
@@ -43,96 +73,8 @@ class ReportesController extends Controller
         //obtener el user api ejemplo en elcontro
         $user = $request->user();
 
-        $respuesta = [
-            "Personales" => [
-                [
-                    "documento_identificacion" => $user->documento_identificacion,
-                    "edad" => $user->edad,
-                    "genero" => $user->genero,
-
-                ]
-            ],
-            "Sociodemograficos" => [
-                [
-                    "estrato" => $user->estrato, // Supongamos que el usuario tiene una propiedad 'estrato'
-                    "nivel_educativo_padre" => $user->nivel_educativo_padre, // Supongamos que el usuario tiene una propiedad 'escolaridad'
-                    "nivel_educativo_madre" => $user->nivel_educativo_madre,
-                ]
-            ],
-            "estilos_vida" => [
-                [
-                    "horas_lectura" => $user->horas_lectura, // Supongamos que el usuario tiene una propiedad 'estrato'
-                    "horas_redes_sociales" => $user->horas_redes_sociales, // Supongamos que el usuario tiene una propiedad 'escolaridad'
-                    "horas_entretenimiento" => $user->horas_entretenimiento,
-                    "hora_sueno" => $user->hora_sueno,
-                    "promedio_arte" => $user->promedio_arte,
-                ]
-            ],
-            "saludable" => [
-                [
-                    "promedio_deporte" => $user->promedio_deporte,
-                    "grasas" => $user->grasas,
-                    "alimentos_saludables" => $user->alimentos_saludables,
-                ]
-            ],
-
-            "macrohabilidad_inductiva" => [
-                [
-                    "induccion_general" => $this->buscar_total_subhabilidad("INDUCCIÓN GENERAL"),
-                    "induccion_especifica" => $this->buscar_total_subhabilidad("INDUCCIÓN ESPECÍFICA"),
-                    "total" => $this->buscar_total_subhabilidad("INDUCCIÓN GENERAL") + $this->buscar_total_subhabilidad("INDUCCIÓN ESPECÍFICA"),
-                ]
-            ],
-
-            "macrohabilidad_abductiva" => [
-                [
-                    "comprobacion_hipotesis" => $this->buscar_total_subhabilidad("COMPROBACIÓN DE HIPÓTESIS"),
-                    "uso_probabilidad_incertidumbre" => $this->buscar_total_subhabilidad("USO DE PROBABILIDAD E INCERTIDUMBRE"),
-                    "total" => $this->buscar_total_subhabilidad("COMPROBACIÓN DE HIPÓTESIS") + $this->buscar_total_subhabilidad("USO DE PROBABILIDAD E INCERTIDUMBRE"),
-                ]
-            ],
-
-            "macrohabilidad_deductivo_y_verbal" => [
-                [
-                    "identificacion_analogia" => $this->buscar_total_subhabilidad("IDENTIFICACIÓN DE FALLO POR ANALOGÍA"),
-                    "identificacion_por_fallo_vaguedad" => $this->buscar_total_subhabilidad("IDENTIFICACIÓN DE FALLO POR VAGUEDAD"),
-                    "total" => $this->buscar_total_subhabilidad("IDENTIFICACIÓN DE FALLO POR ANALOGÍA") + $this->buscar_total_subhabilidad("IDENTIFICACIÓN DE FALLO POR VAGUEDAD"),
-                ]
-            ],
-
-            "macrohabilidad_analisis_de_argumentos" => [
-                [
-                    "identificacion_estructura_argumentativa" => $this->buscar_total_subhabilidad("IDENTIFICACIÓN DE ESTRUCTURA ARGUMENTATIVA"),
-                    "identificacion_de_suposicion" => $this->buscar_total_subhabilidad("IDENTIFICACIÓN DE SUPOSICIÓN"),
-                    "identificacion_de_falacia" => $this->buscar_total_subhabilidad("IDENTIFICACIÓN DE FALACIA"),
-                    "total" => $this->buscar_total_subhabilidad("IDENTIFICACIÓN DE ESTRUCTURA ARGUMENTATIVA") + $this->buscar_total_subhabilidad("IDENTIFICACIÓN DE SUPOSICIÓN") + $this->buscar_total_subhabilidad("IDENTIFICACIÓN DE FALACIA"),
-                ]
-            ],
-            "macrohabilidad_toma_desiciones_y_resolucion_problemas" => [
-                [
-                    "toma_desiciones_informadas" => $this->buscar_total_subhabilidad("TOMA DE DECISIONES INFORMADAS"),
-                    "conciencia_situacion_acciones_razonables" => $this->buscar_total_subhabilidad("CONCIENCIA DE SITUACIÓN Y ACCIONES RAZONABLES"),
-                    "pensamiento_estrategico" => $this->buscar_total_subhabilidad("PENSAMIENTO ESTRATÉGICO"),
-                    "pensamiento_creativo" => $this->buscar_total_subhabilidad("PENSAMIENTO CREATIVO"),
-                ],
-                "metacognicion_conocimiento_procedimental" => [
-                    [
-                        "conocimiento_procedimental" => $request->conocimiento_procedimental,
-                        "depuracion" => $request->depuracion,
-                        "evaluacion" => $request->evaluacion,
-                        "monitoreo" => $request->monitoreo,
-                        "organizacion" => $request->organizacion,
-                        "planificacion" => $request->planificacion,
-                        "total"=>  $request->conocimiento_procedimental + $request->depuracion + $request->evaluacion + $request->monitoreo + $request->organizacion + $request->planificacion
-                    ]
-                ],
-            ]
-
-
-
-        ];
-
-        return view('reporte.index', compact($respuesta));
+     
+        return view('reporte.index', compact());
     }
 
 
--- database/migrations/2024_08_03_155631_create_reportes_table.php
+++ database/migrations/2024_08_03_155631_create_reportes_table.php
@@ -17,11 +17,56 @@ return new class extends Migration
             $table->integer('calificacion_total');
             $table->integer('calificacion_metacognicion');
             $table->date('fecha_calificacion');
+            
+            $table->string('documento_identificacion')->nullable();
+            $table->integer('edad')->nullable();
+            $table->integer('estrato')->nullable();
+            $table->string('nivel_escolaridad')->nullable();
+            $table->string('nivel_educativo_padre')->nullable();
+            $table->string('nivel_educativo_madre')->nullable();
+            $table->string('horas_lectura')->nullable(); 
+            $table->string('horas_redes_sociales')->nullable(); 
+            $table->string('horas_entretenimiento')->nullable(); 
+            $table->string('hora_sueno')->nullable(); 
+            $table->string('genero')->nullable(); 
+            $table->string('promedio_deporte')->nullable();
+            $table->string('promedio_arte')->nullable();
+            $table->string('grasas')->nullable();
+            $table->string('alimentos_saludables')->nullable();
+            $table->string('litro_agua')->nullable();
+            $table->integer('induccion_general')->nullable();
+            $table->integer('induccion_especifica')->nullable();
+            $table->integer('total_macrohabilidad_inductiva')->nullable();
+            $table->integer('comprobacion_hipotesis')->nullable();
+            $table->integer('uso_probabilidad_incertidumbre')->nullable();
+            $table->integer('total_macrohabilidad_abductiva')->nullable();
+            $table->integer('identificacion_analogia')->nullable();
+            $table->integer('identificacion_por_fallo_vaguedad')->nullable();
+            $table->integer('total_macrohabilidad_deductivo_y_verbal')->nullable();
+            $table->integer('identificacion_estructura_argumentativa')->nullable();
+            $table->integer('identificacion_de_suposicion')->nullable();
+            $table->integer('identificacion_de_falacia')->nullable();
+            $table->integer('total_macrohabilidad_analisis_de_argumentos')->nullable();
+            $table->integer('toma_desiciones_informadas')->nullable();
+            $table->integer('conciencia_situacion_acciones_razonables')->nullable();
+            $table->integer('pensamiento_estrategico')->nullable();
+            $table->integer('pensamiento_creativo')->nullable();
+            $table->integer('macrohabilidad_toma_desiciones_y_resolucion_problemas')->nullable();
+            $table->integer('conocimiento_procedimental')->nullable();
+            $table->integer('depuracion')->nullable();
+            $table->integer('evaluacion')->nullable();
+            $table->integer('monitoreo')->nullable();
+            $table->integer('organizacion')->nullable();
+            $table->integer('planificacion')->nullable();
+            $table->integer('total_metacognicion')->nullable();
+
+            
             $table->timestamps();
             
             $table->foreign('id_usuario')->references('id_usuario')->on('users');
         });
     }
+
 
     /**
      * Reverse the migrations.
