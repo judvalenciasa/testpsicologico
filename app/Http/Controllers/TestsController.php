654e5e51da5675d994896510cb26bad3382b70be
Yorman Valencia <yormanvale@outlook.com>
2024-09-11 13:41:11 -0500

    preguntas arregladas

--- app/Http/Controllers/TestsController.php
+++ app/Http/Controllers/TestsController.php
@@ -81,9 +81,12 @@ class TestsController extends Controller
       
         
         $user = Auth::user();
+
+        Log::info($request);
         
         // Verificar si es la primera vez que se carga la página o si ya se han enviado respuestas
         if (!$request->has('pregunta_ids')) {
+
             
             $hora_inicio_prueba = Carbon::now();
             Log::info("cronometro inicio". $hora_inicio_prueba);
@@ -114,8 +117,9 @@ class TestsController extends Controller
                 // Obtener el tipo de pregunta
                 $tipo_pregunta = Preguntas::where('id_pregunta', $pregunta_id)->pluck('tipo_pregunta')->first();
 
+
                 // Procesar preguntas abiertas
-                if ($tipo_pregunta == 'abierta' && $request->has('respuestas_abiertas' . $pregunta_id)) {
+                if ($tipo_pregunta == 'abierta' && $request->has('respuestas_abiertas')) {
                     $respuesta_abierta = $request->input('respuestas_abiertas.' . $pregunta_id);
 
                     if (!is_string($respuesta_abierta) || empty(trim($respuesta_abierta))) {
@@ -146,9 +150,8 @@ class TestsController extends Controller
                 }
 
                 // Procesar preguntas cerradas
-                if ($tipo_pregunta == 'cerrada' && $request->has('respuestas_cerrada' . $pregunta_id)) {
+                if ($tipo_pregunta == 'cerrada' && $request->has('respuestas_cerrada')) {
                     $respuesta_cerrada = $request->input('respuestas_cerrada.' . $pregunta_id);
-
                     $opcionSeleccionada = DB::table('opciones')
                         ->where('id_pregunta', $pregunta_id)
                         ->where('id_opcion', $respuesta_cerrada)
