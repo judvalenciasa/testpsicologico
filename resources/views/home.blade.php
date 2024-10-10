8baf504ddc252b03062a9e67abfb5238679489f9
Yorman Valencia <yormanvale@outlook.com>
2024-10-09 22:45:54 -0500

    cambios en home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>CognitiveSparks | Home</title>
+    <title>CognitiveSparks</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
