1a21fdafd0fa96f02d362033adac0371a9f1e5fb
Yorman Valencia <yormanvale@outlook.com>
2024-11-21 19:53:17 -0500

    email con mailgun

--- app/Mail/MiMailable.php
+++ app/Mail/MiMailable.php
@@ -22,7 +22,7 @@ class MiMailable extends Mailable
 
     public function build()
     {
-        return $this->from('example@example.com')
+        return $this->from('no-reply@cognitivespark.com')
             ->subject('Credenciales CognitiveSpark')
             ->html('
                 <div style="font-family: Arial, sans-serif; color: #333;">
